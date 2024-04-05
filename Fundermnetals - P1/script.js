135c6d237c092463870da7bd0a291872962e8c1b
Lucy Ahadzi <lucyahadzi@gmail.com>
2024-04-05 14:11:55 +0000

    My Commit

--- Fundermnetals - P1/script.js
+++ Fundermnetals - P1/script.js
@@ -30,8 +30,7 @@ age = 31;
 const birthYear = 1997;
 
 var job = "programmer";
-job = "teacher"; */
-
+job = "teacher"; 
 const currentYear = 2037;
 const ageJonas = currentYear -1995;
 const ageLulu = currentYear - 1997;
@@ -54,5 +53,43 @@ console.log(x);
 
 //Comparison Operators
 console.log(ageJonas > ageLulu);
-console.log(ageLulu >= 18);
+console.log(ageLulu >= 18); 
 
+
+//Precedence Operator
+const currentYear = 2037;
+const ageJonas = currentYear -1995;
+const ageLulu = currentYear - 1997;
+
+console.log(currentYear - 1995 > currentYear - 1997);
+
+console.log(15 - 3 - 14);
+
+let x, y;
+x = y = 15 - 3 - 14;
+console.log(x,y);
+
+
+const averageAge = (ageJonas + ageLulu) / 2;
+console.log(ageJonas, ageLulu, averageAge);*/
+
+//Coding Challenge #1
+
+const markMass = 78;
+const johnMass = 92;
+const markHeight = 1.69;
+const johnHeight = 1.95;
+const markBMI = markMass / (markHeight * markHeight);
+const johnBMI = johnMass / (johnHeight * johnHeight);
+const markHigherBMI = markBMI > johnBMI;
+console.log(Math.floor(johnBMI),Math.floor(markBMI),markHigherBMI);
+
+//Data 2
+const markMassTwo = 95;
+const markHeightTwo = 1.88;
+const johnMassTwo = 85;
+const johnHeightTwo = 1.76;
+const markBMITwo = markMassTwo / (markHeightTwo * markHeightTwo);
+const johnBMITwo = johnMassTwo/ (johnHeightTwo * johnHeightTwo);
+const markHigherBMITwo = markBMITwo > johnBMITwo;
+console.log(Math.floor(markBMITwo), Math.floor(johnBMITwo), markHigherBMITwo);
