956c992316ed922ca71c66745bf8795b712e49e0
Lucy Ahadzi <lucyahadzi@gmail.com>
2024-04-05 13:09:54 +0000

    My Commit

--- Fundermnetals - P1/script.js
+++ Fundermnetals - P1/script.js
@@ -1,2 +1,58 @@
-let js = "amazing";
+/*let js = "amazing";
 console.log(40 + 8 + 23 - 10);
+console.log("Jonas");
+console.log("23");
+
+let firstName = "Jonas";
+console.log(firstName); 
+
+true;
+console.log(true);
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+javaScriptIsFun = "Yes!";
+console.log(typeof javaScriptIsFun);
+let year;
+console.log(year);
+console.log(typeof year);
+year = 1997;
+console.log(typeof year); 
+
+let age = 30;
+age = 31;
+
+const birthYear = 1997;
+
+var job = "programmer";
+job = "teacher"; */
+
+const currentYear = 2037;
+const ageJonas = currentYear -1995;
+const ageLulu = currentYear - 1997;
+console.log(ageJonas, ageLulu);
+
+console.log (ageJonas * 3, ageJonas/ 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Lucy";
+const lastName = "Ahadzi";
+console.log(firstName + " " +lastName);
+
+//Assignment Operators
+let x = 10 + 5;
+x += 10; 
+x *= 4;
+x++;
+x--;
+console.log(x);
+
+//Comparison Operators
+console.log(ageJonas > ageLulu);
+console.log(ageLulu >= 18);
+
