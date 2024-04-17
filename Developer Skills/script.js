1ef45ec5e41838c51c51f8ef2f24c5d3d7cf4ac2
Lucy Ahadzi <lucyahadzi@gmail.com>
2024-04-17 13:27:56 +0000

    My Commit

--- Developer Skills/script.js
+++ Developer Skills/script.js
@@ -11,25 +11,44 @@ const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 
 //2. Breaking up into sub-problems
 //- How to ignore error?
-//- Find max value in tem array
+//- Find max value in temp array
 //-Find min value in temp array
 // Subtract min from max (amplitude) and return it
 
-function calcTempAmplitude(temps) {
-  let max = temps[0];
-  let min = temps[0];
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if(typeof curTemp !== "number") continue;
+// function calcTempAmplitude(temps) {
+//   let max = temps[0];
+//   let min = temps[0];
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if(typeof curTemp !== "number") continue;
 
-    if (curTemp > max) max = curTemp;
-    if(curTemp < min) min = curTemp;
-  }
-  console.log(max, min);
-  return max - min;
-}
+//     if (curTemp > max) max = curTemp;
+//     if(curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// }
 
-const amplitude = calcTempAmplitude(temperatures);
-console.log(amplitude);
+// const amplitude = calcTempAmplitude(temperatures);
+// console.log(amplitude);
 
 // Problem Two
+
+// Coding Challenge #1
+
+// 1. Understanding the Problem
+//Array of strings print the forecast for the upcoming days
+
+let tempList = [17, 21, 23];
+function printForecast(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(
+      `${arr[i]} degrees celsius in ${i + 1} day${i === 0 ? "" : "s"}...`
+    );
+  }
+}
+printForecast(tempList);
+
+console.log("------DATASET TWO-------")
+tempList = [12, 5, -5, 0, 4];
+console.log(printForecast(tempList));
